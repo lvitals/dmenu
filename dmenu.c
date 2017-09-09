ef9e6ff79066be44db6380632d9d00b7bebd5948
Leandro Vital <leavitals@gmail.com>
2017-09-09 18:33:08 -0300

    Fix key tab and left shift + tab

--- dmenu.c
+++ dmenu.c
@@ -468,21 +468,19 @@ keypress(XKeyEvent *ev)
 		if (lines > 0)
 			return;
 		/* fallthrough */
-	case XK_Down:
-		if (sel && sel->right && (sel = sel->right) == next) {
-			curr = next;
-			calcoffsets();
-		}
-		break;
 	case XK_Tab:
-		if (!sel)
-			return;
-		strncpy(text, sel->text, sizeof text - 1);
-		text[sizeof text - 1] = '\0';
-		cursor = strlen(text);
-		fuzzymatch();
-		break;
-	}
+        if(sel && sel->right && (sel = sel->right) == next) {
+            curr = next;
+            calcoffsets();
+        }
+        break;
+    case XK_ISO_Left_Tab:
+        if(sel && sel->left && (sel = sel->left)->right == curr) {
+            curr = prev;
+            calcoffsets();
+        }
+        break;
+    }
 	drawmenu();
 }
 
